1bd35f9d02b4d90c91d8d2f23e2b8be9fec1da29
Moznur Rahman <rmoznu82@gmail.com>
2025-01-02 23:22:31 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$('.sliderItem').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<i class="left fas fa-chevron-left"></i><',
+  nextArrow: '<i class="right fas fa-chevron-right"></i>',
+dots:true,
+});
+
+  
